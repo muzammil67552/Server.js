d54a2125033c42b3c5ce38a5d71b056c7a0383ba
Muhammad Muzammil <yoursmuzammil16070@gmail.com>
2024-12-14 21:03:49 +0500

    first commit

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
-const express = require("express")
-const  cors = require("cors")
-require("dotenv").config();
+const express = require ("express");
+const cors = require ("cors");
+require("dotenv").config()
 
-const app = express();
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
+const app = express()
 
-app.use(cors());
+app.use(cors())
 app.use(express.json())
 
-app.get("/" , (req, res) =>{
-    res.send("Hello World")
-}) 
-app.listen(PORT, () => {
-    console.log(`Server is Listen on ${PORT}`)
+app.listen (PORT, (req, res) =>{
+    console.log(`Server is Listen ${PORT}`);
+    
 })
-
