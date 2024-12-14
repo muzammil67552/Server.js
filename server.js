4db9b68d6a5499d5a60745bd2c5ada0bbfd26473
Muhammad Muzammil <yoursmuzammil16070@gmail.com>
2024-12-14 23:36:13 +0500

    first commit

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 const express = require ("express");
 const cors = require ("cors");
+const { default: mongoose } = require("mongoose");
 require("dotenv").config()
 
 const PORT = process.env.PORT || 5000
 const app = express()
 
+//middleware
 app.use(cors())
 app.use(express.json())
 
+
+//mongoose Connection
+mongoose
+.connect(process.env.MONGO_URI)
+.then(() =>{
+    console.log("coonected Successfully")
+})
+.catch((err) =>{
+    console.log("error in connecting")
+})
+
+
+app.get('/', (req, res) =>{
+    res.send("Hello World")
+})
 app.listen (PORT, (req, res) =>{
     console.log(`Server is Listen ${PORT}`);
     
